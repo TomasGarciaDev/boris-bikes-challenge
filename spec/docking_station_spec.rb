7f839a9356f3db79fd5d22322f7c3b34c11a796d
Tomas Garcia <tomasgarciadev@gmail.com>
2022-02-02 12:35:19 +0100

    Step 10, DockingStation releasing working bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,14 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it 'releases a bike instance' do
+    bike = subject.release_bike
+    expect(bike).to be_instance_of(Bike)
+  end
+
+  it 'can check if the released bike instance is working?' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
