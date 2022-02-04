d78cae6caa84a5709ef76a06c31ced374b66c392
Tomas Garcia <tomasgarciadev@gmail.com>
2022-02-04 18:04:19 +0100

    Step #15, refactoring, private methods full? and empty? added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,22 @@ class DockingStation
   end
 
   def release_bike
-    raise "Sorry, this Docking Station is empty" if @bikes.empty?
+    raise "Sorry, this Docking Station is empty" if empty?
     @bikes.pop()
   end
 
   def dock_bike(bike)
-     raise "Sorry, this Docking Station is full" if @bikes.length  >= 20
+     raise "Sorry, this Docking Station is full" if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.length  >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
